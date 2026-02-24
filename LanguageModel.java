d21f34207ad43a1e6995e4cba9f22b13ef7cf967
Yoav Rubin <yoav.rubin@post.runi.ac.il>
2026-02-24 23:26:43 +0200

    v03

--- LanguageModel.java
+++ LanguageModel.java
@@ -106,7 +106,7 @@ public class LanguageModel {
     StringBuilder generatedText = new StringBuilder(initialText);
     String window = initialText.substring(initialText.length() - windowLength);
 
-    while (generatedText.length() < textLength) {
+    while (generatedText.length() < initialText.length() + textLength) {
         List probs = CharDataMap.get(window);
         if (probs == null) {
             break;
